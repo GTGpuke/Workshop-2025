5ddba64204f5d5d651e4f4d9e06688d83a2c6eb6
leachas <lea.chassagnard@gmail.com>
2025-10-15 12:55:35 +0200

    fix coverage reports

--- N_05/fond_transparent/script.js
+++ N_05/fond_transparent/script.js
@@ -69,10 +69,10 @@ function init() {
 
 function animate() {
   ctx.clearRect(0, 0, canvas.width, canvas.height);
-  particlesArray.forEach((particle) => {
+  for (const particle of particlesArray) {
     particle.update();
     particle.draw();
-  });
+  }
   requestAnimationFrame(animate);
 }
 
