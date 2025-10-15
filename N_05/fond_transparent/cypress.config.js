a819928120fdb6eec13be3fedfdfe537d5776207
leachas <lea.chassagnard@gmail.com>
2025-10-15 11:06:05 +0200

    fix 4

--- N_05/fond_transparent/cypress.config.js
+++ N_05/fond_transparent/cypress.config.js
@@ -1,7 +1,7 @@
 // N_05/fond_transparent/cypress.config.js
 
 const { defineConfig } = require('cypress');
-const { addMatchImageSnapshotPlugin } = require('cypress-image-snapshot/plugin');
+const { addMatchImageSnapshotPlugin } = require('@simonsmith/cypress-image-snapshot/plugin');
 
 module.exports = defineConfig({
   e2e: {
